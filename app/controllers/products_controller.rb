a874b03861641b29392650990448b9b2dee8b228
brianaalexander <bribri0283@gmail.com>
2022-08-30 20:45:29 -0500

    added shortcut to adding routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,23 +4,8 @@ class ProductsController < ApplicationController
     render json: all.as_json
   end
 
-  def first_product
-    first = Product.first
-    render json: first.as_json
-  end
-
-  def second_product
-    second = Product.second
-    render json: second.as_json
-  end
-
-  def third_product
-    third = Product.third
-    render json: third.as_json
-  end
-
-  def fourth_product
-    fourth = Product.fourth
-    render json: fourth.as_json
+  def one_product_method
+    product = Product.find_by(id: params["id"])
+    render json: product.as_json
   end
 end
