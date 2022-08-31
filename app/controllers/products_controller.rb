ed11bcbb047ed82229b38c8d3e566c98014c994e
brianaalexander <bribri0283@gmail.com>
2022-08-31 18:56:18 -0500

    added create RESTful

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,15 @@ class ProductsController < ApplicationController
     render json: all.as_json
   end
 
+  def create
+    product = Product.new(name: "jacket",
+                          price: 25,
+                          image_url: "#",
+                          description: "wear when you are cold")
+    product.save
+    render json: product.as_json
+  end
+
   def show
     product = Product.find_by(id: params["id"])
     render json: product.as_json
