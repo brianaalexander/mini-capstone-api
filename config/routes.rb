76b6702a46fba3f8df12a3520fa519cf258ce6f9
brianaalexander <bribri0283@gmail.com>
2022-08-31 18:36:02 -0500

    Restful updates

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  get "/one_product/:id", controller: "products", action: "one_product_method"
   get "/products" => "products#index"
   get "/products/:id" => "products#show"
 end
