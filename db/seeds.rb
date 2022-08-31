9657ba50167e3468357860bf9609f77fb3f84fa4
brianaalexander <bribri0283@gmail.com>
2022-08-30 19:01:37 -0500

    products made

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+product = Product.new(name: "dresses", price: 10, image_url: "#", description: "summer wear")
+product.save
+product = Product.new(name: "pants", price: 15, image_url: "#", description: "long shorts")
+product.save
+product = Product.new(name: "shorts", price: 8, image_url: "#", description: "short pants")
+product.save
+product = Product.new(name: "shoes", price: 20, image_url: "#", description: "feet covers")
+product.save
