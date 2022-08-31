dc9165ac92aa05091c7fa63a9437b0466c589a02
brianaalexander <bribri0283@gmail.com>
2022-08-30 19:52:49 -0500

    updated controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,26 +1,26 @@
 class ProductsController < ApplicationController
   def all_products
-    all = Products.all
+    all = Product.all
     render json: all.as_json
   end
 
   def first_product
-    first = Products.first_product
+    first = Product.first
     render json: first.as_json
   end
 
   def second_product
-    second = Products.second_product
+    second = Product.second
     render json: second.as_json
   end
 
   def third_product
-    third = Products.third_product
+    third = Product.third
     render json: third.as_json
   end
 
   def fourth_product
-    fourth = Products.fourth_product
+    fourth = Product.fourth
     render json: fourth.as_json
   end
 end
