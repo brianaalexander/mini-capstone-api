e4004b105f8264bb377458d973b9b8b14f220a47
brianaalexander <bribri0283@gmail.com>
2022-09-01 21:25:31 -0500

    made product instant variable

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,7 +10,8 @@ class ProductsController < ApplicationController
                           image_url: params["image_url"],
                           description: params["description"])
     product.save
-    render json: product.as_json
+    @product = product
+    render template: "products/create"
   end
 
   def show
@@ -25,7 +26,8 @@ class ProductsController < ApplicationController
     product.image_url = params["image_url"] || product.image_url
     product.description = params["description"] || product.description
     product.save
-    render json: product.as_json
+    @product = product
+    render template: "products/update"
   end
 
   def destroy
