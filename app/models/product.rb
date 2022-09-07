93c0c694eb97086cd91da821ef744b768a442fdb
brianaalexander <bribri0283@gmail.com>
2022-09-06 20:50:18 -0500

    added has_many and belongs

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,9 +6,10 @@ class Product < ApplicationRecord
   validates :price, numericality: { greater_than: 0 }
   validates :description, presence: true
 
-  def supplier
-    Supplier.find_by(id: supplier_id)
-  end
+  belongs_to :supplier
+  # def supplier
+  #   Supplier.find_by(id: supplier_id)
+  # end
 
   def friendly_created_at
     created_at.strftime("%B %e, %Y")
